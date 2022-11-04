3eb4cf0e1b7f68914df27d0c9ab42ca5869b2743
alex <source@proof.construction>
2022-11-04 17:11:03 -0500

    who knows

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
 FROM ubuntu:latest
 ENV DEBIAN_FRONTEND=noninteractive
-ENV VIRTUAL_ENV=/opt/venv
+ENV VIRTUAL_ENV=/app/venv
 RUN apt update && apt upgrade -y
 RUN apt install -y build-essential python3-venv python3-dev git
-COPY requirements.txt .
+RUN apt install -y ffmpeg libsm6 libxext6
+COPY . /app
+WORKDIR /app
 RUN git clone https://github.com/sparkfish/augraphy
 RUN python3 -m venv $VIRTUAL_ENV
 ENV PATH="$VIRTUAL_ENV/bin:$PATH"
 RUN pip install -r augraphy/requirements-dev.txt
 RUN pip install -r requirements.txt
-CMD uvicorn main:app --host=0.0.0.0 --workers=2
+EXPOSE 8000
+CMD uvicorn app:app --host=0.0.0.0 --workers=2
--- app.py
+++ app.py
@@ -1,31 +1,37 @@
 from fastapi import FastAPI, Form, File, UploadFile
-from augraphy import AugraphyPipeline
+from augraphy import *
 import cv2
 
 app = FastAPI()
 
 @app.post("/crappify")
 def crappify(
-    inkPhase: list[str] = [],
-    paperPhase: list[str] = [],
-    postPhase: list[str] = [],
+    # inkPhase: list[str] = [],
+    # paperPhase: list[str] = [],
+    # postPhase: list[str] = [],
     imageFile: bytes = File(),
 ):
-    inks = []
-    papers = []
-    posts = []
-    for s in inkPhase:
-        inks += globals()[s]
-    for s in paperPhase:
-        papers += globals()[s]
-    for s in postPhase:
-        posts += globals()[s]
+
+    # turn the lists of augmentation names into augmentation functions
+    # inks = []
+    # papers = []
+    # posts = []
+    # for s in inkPhase:
+    #     inks += globals()[s]
+    # for s in paperPhase:
+    #     papers += globals()[s]
+    # for s in postPhase:
+    #     posts += globals()[s]
+
+    # save the file to tmp
+    with open("imageFile.png", "wb") as f:
+        f.write(imageFile)
 
     # by now we have a list of augmentations per phase,
     # so we produce the final pipeline
-    pipeline = AugraphyPipeline(inks, papers, posts)
+    # pipeline = AugraphyPipeline(inks, papers, posts)
 
     # now we run the pipeline over the image
-    img = cv2.imread(imageFile.file)
-    output = pipeline.augment(img)
-    return output
+    img = cv2.imread("imageFile.png")
+    crappified = default_augraphy_pipeline().augment(img)
+    cv2.imwrite("imagefile-crappified.png", crappified["output"])
